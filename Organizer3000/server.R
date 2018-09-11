fdb7936861387bc89bf35919a3f210da92db358a
jlondonob <43001823+jlondonob@users.noreply.github.com>
2018-09-10 21:06:14 -0500

    Desarrolla Info. Personal
    Con esta actualización la aplicación logra imprimir la información personal de cada uno de los trabajadores

--- Organizer3000/server.R
+++ Organizer3000/server.R
@@ -5,14 +5,32 @@ library(readxl)
 #Se crea servidor
 shinyServer(function(input, output) {
   
-  #Se crea elemento reactivo que lee la tabla de datos importada anteriormente
+#-------------------------- (1) Creación de tablas de Datos-------------------------#
+    
+  #Se crea elemento reactivo para tabla sobre info. personal
+  #Esta tabla solo tiene en cuenta características personales eliminando duplicados.
+  pers <- reactive({
+    inDile <- input$datos
+    ppl<- read_xlsx(inDile$datapath, skip=2)[,c("Tercero","Nombre del Tercero","Precio/Valor Bruto","Cargo","Correo Electronico", "Centro de Costos")]
+    ppl <- ppl[!duplicated(ppl$`Nombre del Tercero`),]
+    return(ppl)
+  })
+  
+  #Se crea elemento reactivo que lee la tabla de datos para discriminar transacciones.
   dat<-reactive({
     inFile<- input$datos
-    datos<-read_xlsx(inFile$datapath, sheet= 1) #datapath se crea automaticamente y es la dirección del documento.
+    #Notese que tengo (skip=2), entonces, siempre se tienen que poner los nombres de las columanas
+    #en la segunda fila.
+    
+    datos<-read_xlsx(inFile$datapath, sheet= 1, skip = 2)[,c("Nro. Registro","Nombre del Tercero","Nombre Transacción","Tipo Asiento","Precio/Valor Bruto")] #datapath se crea automaticamente y es la dirección del documento.
     return(datos)
   })
+  
   #Se crea elemento reactivo que contiene una lista con los nombres únicos
-  list_nom <- reactive({levels(factor(dat()$Nombre))})
+  list_nom <- reactive({levels(factor(dat()$`Nombre del Tercero`))})
+  
+  
+ #-------------------------- (2) Creacion de los PDF ----------------------------# 
   
   #Se desarrolla el documento que será descargado por el usuario
   output$db <- downloadHandler(
@@ -23,17 +41,20 @@ shinyServer(function(input, output) {
       
       sf<-c()
       for(i in list_nom()){
-      sink(paste0(i,".Rmd"))
-      cat("Nombre:", i, "\n") #La "\n" crea una nueva linea.
-      sink()
-      rmarkdown::render(paste0(i,".Rmd"), 
-                        output_format = "pdf_document")
-      unlink(paste0(i,".Rmd"))
-      sf<-c(sf,paste0(i,".pdf"))
+     
+        
+        rmarkdown::render("plantilla1.Rmd", 
+                        output_format = "pdf_document",
+                        output_file = paste0(i,".pdf"))
+        setwd
+      sf<-c(sf, paste0(i,".pdf"))
+      }
+      for(i in list_nom()){
+        sf <- c(sf,  paste0(i,".pdf"))
       }
     zip(zipfile = file, files = sf)
-    }
     
+    }
     
     
   )
